664a71eaad0a0e49f0d1d3a1d00dbbf90bc20fa3
David Freitag <freitagdavid@outlook.com>
2022-01-27 14:07:55 -0600

    components/ItemList/index: Big rework

--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -1,23 +1,39 @@
-import styled from "styled-components";
 import { useState } from "react";
 import Item from "../Item";
 import { Paper } from "@mui/material";
-
-const StyledItemList = styled.div`
-    width: 50%;
-    flex: 1;
-`;
+import { Box } from "@mui/system";
+import { motion } from "framer-motion";
+import ItemInput from "../itemInput";
 
 const ItemList = () => {
     const [items, setItems] = useState([
-        { label: "Item 1", price: 100, category: "entertainment", votes: 0 },
-        { label: "Item 2", price: 100, category: "entertainment", votes: 0 },
-        { label: "Item 3", price: 100, category: "entertainment", votes: 0 },
+        {
+            id: 0,
+            label: "Item 1",
+            price: 100,
+            category: "entertainment",
+            votes: 0,
+        },
+        {
+            id: 1,
+            label: "Item 2",
+            price: 100,
+            category: "entertainment",
+            votes: 0,
+        },
+        {
+            id: 2,
+            label: "Item 3",
+            price: 100,
+            category: "entertainment",
+            votes: 0,
+        },
     ]);
 
     const vote = (index, vote) => {
         setItems((prevItems) => {
-            if (prevItems[index].votes <= 0) {
+            console.log("test");
+            if (prevItems[index].votes <= 0 && vote === -1) {
                 return prevItems;
             }
             const newItems = [...prevItems];
@@ -27,17 +43,57 @@ const ItemList = () => {
         });
     };
 
+    const spring = {
+        type: "springt",
+        damping: 20,
+        stiffness: 300,
+    };
+
     return (
-        <StyledItemList>
-            <Paper style={{ height: "100%" }}>
-                {items.map((item, index) => (
-                    <Item key={index} index={index} {...item} vote={vote}>
-                        {JSON.stringify(item)}
-                    </Item>
-                ))}
+        <Box
+            sx={{
+                width: "100%",
+                height: "100vh",
+                padding: "10px",
+            }}
+        >
+            <Paper
+                sx={{
+                    height: "100%",
+                    width: "100%",
+                    paddingX: 10,
+                    paddingY: 5,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                }}
+                elevation={2}
+            >
+                <Box sx={{ overflowY: "scroll" }}>
+                    {items.map((item, index) => (
+                        <motion.div key={item.id} layout transition={spring}>
+                            <Item index={index} {...item} vote={vote} />
+                        </motion.div>
+                    ))}
+                </Box>
+                <Box>
+                    <ItemInput></ItemInput>
+                </Box>
             </Paper>
-        </StyledItemList>
+        </Box>
     );
+
+    // return (
+    //     <StyledItemList>
+    //         <Paper style={{ height: "100%" }}>
+    //             {items.map((item, index) => (
+    //                 <Item key={index} index={index} {...item} vote={vote}>
+    //                     {JSON.stringify(item)}
+    //                 </Item>
+    //             ))}
+    //         </Paper>
+    //     </StyledItemList>
+    // );
 };
 
 export default ItemList;
