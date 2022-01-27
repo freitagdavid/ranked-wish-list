6678c9c0e65565dc31980de5620268ab96aec5d8
David Freitag <freitagdavid@outlook.com>
2022-01-27 14:07:28 -0600

    components/Item/index: totally reworked it

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,26 +1,79 @@
-import { Card, Fab, IconButton } from "@mui/material";
+import { Box, Card, Fab, Grid, IconButton } from "@mui/material";
 import styled from "styled-components";
 import { ExpandMore, ExpandLess, Delete } from "@mui/icons-material";
 
-const Item = ({ label, price, category, index, className, votes, vote }) => {
-    return (
-        <Card className={className}>
-            <IconButton onClick={() => vote(index, 1)}>
-                <ExpandLess />
-            </IconButton>
-            <p>{votes}</p>
-            <IconButton onClick={() => vote(index, -1)}>
-                <ExpandMore />
-            </IconButton>
-            <p>{label}</p>
-            <p>{price}</p>
-            <p>{category}</p>
-            <IconButton color="error">
-                <Delete />
-            </IconButton>
-        </Card>
-    );
-};
+const Item = ({ label, price, category, index, className, votes, vote }) => (
+    <Card className={className} sx={{ height: 50, width: "100%" }}>
+        <Grid container spacing={2}>
+            <Grid
+                item
+                xs={1}
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <IconButton onClick={() => vote(index, 1)}>
+                    <ExpandLess />
+                </IconButton>
+            </Grid>
+            <Grid
+                item
+                xs={1}
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+            >
+                <p>{votes}</p>
+            </Grid>
+            <Grid
+                item
+                xs={1}
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <IconButton onClick={() => vote(index, -1)}>
+                    <ExpandMore />
+                </IconButton>
+            </Grid>
+            <Grid item xs={5}>
+                <p>{label}</p>
+            </Grid>
+            <Grid
+                item
+                xs={1}
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <p>{price}</p>
+            </Grid>
+            <Grid item xs={2}>
+                <p>{category}</p>
+            </Grid>
+            <Grid
+                item
+                xs={1}
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <IconButton color="error">
+                    <Delete />
+                </IconButton>
+            </Grid>
+        </Grid>
+    </Card>
+);
 
 const StyledItem = styled(Item)`
     display: flex;
